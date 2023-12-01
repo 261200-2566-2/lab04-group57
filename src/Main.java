2de80ba3e87f511592c326f975f28312659a5edc
SukriTM <micsu.ky@gmail.com>
2023-12-01 21:19:58 +0700

    added lab04 Designing Interfaces

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,22 @@
+//
+//lab04: Designing Interfaces
+//Character > [Archer] [Swordsman]
+//Accessories > [Bracelet] [Cloaks]
+//
+//[Character]
+//Archer; atk=15 def=7 hp=15 spd=10
+//Swordsman; atk=10 def=10 hp=25 spd=7
+//attack(object); attack at the target
+//equip(Accessories); equip Accessories
+//unequip(Accessories); unequip accessories in accessories set
+//skill(target,skillset); use skill in skill set
+//
+//[Accessories]
+//Bracelet; atk+10, spd+10
+//Cloaks; spd+15
+//Upgrade; level up accessory
+//Check stat; return every status in array format [level,hp,atk,def,mp,spd,]
+//
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
